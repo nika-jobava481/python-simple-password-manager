4997843baa32e01cd3341c2f3795d6d8e0f96396
nika-jobava481 <jobavanika481@gmail.com>
2023-01-05 22:17:00 +0400

    added little bit cryptography

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import getpass
+from cryptography.fernet import Fernet
 
 master_password=getpass.getpass("Master password: ")
+
+        
+def get_key():
+    with open("key.key","rb") as keyFile:
+        return keyFile.read()
+
+fer=Fernet(get_key() + master_password.encode())
+
+# def set_key():
+#     key=Fernet.generate_key()
+#     with open("key.key","wb") as keyFile:
+#         keyFile.write(key)
+# set_key()
+
 
 def get():
     soc=input("website/app name: ").lower()
@@ -8,14 +23,15 @@ def get():
         for line in f.readlines():
             print(line.rstrip().split(" | ")[0].lower())
             if line.rstrip().split(" | ")[0].lower() == soc:
-                print(line)
+                web,user,passwd=line.rstrip().split(" | ")
+                print(f"for {web} Username: {user} password is \"{fer.decrypt(passwd.encode()).decode()}\"")
 
 def add():
     soc=input("website/app name: ")
     username=input("account username: ")
     password=getpass.getpass("account password: ")
     with open("passwords.txt","a") as f:
-        f.write(f"{soc} | {username} | {password}\n")
+        f.write(f"{soc} | {username} | {fer.encrypt(password.encode()).decode()}\n")
 
 
 while True:
